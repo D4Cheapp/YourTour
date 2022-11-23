a6033ee9aef9fa811361df0eee53c2c870d8cfcb
D4Cheap <hitrojopic@gmail.com>
2022-11-24 00:43:23 +0300

    Initial commit with webpack building

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports={
+  mode: 'development',
+  entry: path.resolve(__dirname,'./src/js/app.js'),
+  devServer: {
+    historyApiFallback: true,
+    port:3000,
+    hot:true,
+  },
+  output: {
+    filename: `app.js`,
+    path: path.resolve(__dirname,'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sass|css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+      },{
+      test: /\.(jpg|png|svg|gif|ico)$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: 'images/[name].[ext]'
+        },
+      }]}],
+  },
+  plugins: [new HtmlWebpackPlugin({template: path.resolve(__dirname,'./src/index.html')}),
+   new CleanWebpackPlugin()]
+}
