b13ecfac4e14a3c0ba1358c9b3ee8902600d4a83
D4Cheap <hitrojopic@gmail.com>
2022-11-26 18:04:54 +0300

    Build your tour and reviews blocks

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
-import "./images/Header/HeaderBackground.png";
-import "./images/Header/HeaderLogo.png";
-import "./images/ChooseYourTour/Bus.png";
-import "./images/ChooseYourTour/Hills.png";
-import "./images/ChooseYourTour/City.png";
-import "./images/ChooseYourTour/Desert.png";
-import "./images/ChooseYourTour/Lake.png";
-import "./images/ChooseYourTour/Beach.png";
+import "./images/header/HeaderBackground.png";
+import "./images/header/HeaderLogo.png";
+import "./images/chooseYourTour/Bus.png";
+import "./images/chooseYourTour/Hills.png";
+import "./images/chooseYourTour/City.png";
+import "./images/chooseYourTour/Desert.png";
+import "./images/chooseYourTour/Lake.png";
+import "./images/chooseYourTour/Beach.png";
+import "./images/svg/DropDownArrow.svg";
+import "./images/svg/CardsArrow.svg";
+import "./images/users/Maria.png";
+import "./images/users/Pavel.png";
 import "./sass/style.sass";
 
