9cca68deb29aeb9571c73dca939f4b5171c0ac48
D4Cheap <hitrojopic@gmail.com>
2022-11-29 22:29:27 +0300

    Оптимизация изображений и разбитие index.html на блоки файлов

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackSimpleIncludePlugin = require("html-webpack-simple-include-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const path = require('path');
+const fs = require("fs");
 
 module.exports={
   //Режим проекта и точка входа
   mode: 'development',
   entry: path.resolve(__dirname,'./src/main.js'),
+  //Оптимизация svg imagemin
   optimization: {
     runtimeChunk: 'single',
+    minimizer: [new ImageMinimizerPlugin({
+      minimizer: {
+        implementation: ImageMinimizerPlugin.imageminMinify,
+        options:{plugins: [["svgo",{plugins:[{removeViewBox:false}]}]
+    ]}}})]
   },
   //Настройки сервера
   devServer: {
@@ -39,9 +48,24 @@ module.exports={
         loader: 'file-loader',
         options: {
           name: '[path]/[name].[ext]'
-        },
-      }]}],
+        }},
+        {// Оптимизация всех изображений
+        loader: "webp-loader",
+        options: {
+          quality: 75
+        }
+      }
+      ]}],
   },
   plugins: [new HtmlWebpackPlugin({template: path.resolve(__dirname,'./src/index.html')}),
+    new HtmlWebpackSimpleIncludePlugin([
+        {tag: '<header-block/>',content: fs.readFileSync(path.resolve(__dirname,'./src/html/header.html'))},
+        {tag: '<choose-your-tour/>',content: fs.readFileSync(path.resolve(__dirname,'./src/html/chooseYourTour.html'))},
+        {tag: '<build-your-tour/>',content: fs.readFileSync(path.resolve(__dirname,'./src/html/buildYourTour.html'))},
+        {tag: '<tour-review/>',content: fs.readFileSync(path.resolve(__dirname,'./src/html/tourReviews.html'))},
+        {tag: '<tour-photos/>',content: fs.readFileSync(path.resolve(__dirname,'./src/html/tourPhotos.html'))},
+        {tag: '<tour-history/>',content: fs.readFileSync(path.resolve(__dirname,'./src/html/tourHistory.html'))},
+        {tag: '<footer-and-cta/>',content: fs.readFileSync(path.resolve(__dirname,'./src/html/footerAndCta.html'))},
+        ]),
    new CleanWebpackPlugin()]
 }
